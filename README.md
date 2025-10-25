0b7f5a791d2560e04096fe91c5dc7b6e4f6659f9
Martijn Voncken <mvoncken@gmail.com>
2025-10-25 11:14:49 +0200

    Initial commit: Bare minimum for a functioning owlbear extension

---
+++ README.md
@@ -0,0 +1,5 @@
+## DEV
+
+Local development
+1: `npm run dev`
+2: add http://localhost:5173/manifest.json as an owlbear extension.
---
+++ owl20.js
@@ -0,0 +1,38 @@
+import OBR from "@owlbear-rodeo/sdk";
+
+const sound = new Audio('dice-89594.mp3');
+
+const renderRoll = (html) =>{
+    const element = document.querySelector("#rolls-list");
+    const node = document.createElement("div");
+    node.innerHTML = html;
+    element.appendChild(node); 
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+const broadcastRoll = (request) =>{
+    sound.load();
+    sound.play();      
+    const html = `<h3>${request.character}</h3> ${request.html}`;
+    OBR.broadcast.sendMessage("owl20.roll", html, {destination:"ALL"});
+}
+
+addEventListener("message", (event) => { 
+  //broadcastRoll(event) 
+  switch(event.data.type) {
+    case 'Beyond20_Roll':
+      console.log("Broadcasting roll:", event.data.data);
+      broadcastRoll(event.data.data);
+      break;
+    default:
+      console.log("OWL20-owlbear extension recieved unknown message:", event);
+  }
+})
+
+OBR.onReady(() => {
+  console.log("OBR Ready")
+  OBR.broadcast.onMessage("owl20.roll", (event) => {
+      renderRoll(event.data); 
+  })
+})
+window.OBR = OBR // for debug
