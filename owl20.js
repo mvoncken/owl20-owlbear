4c0a9427546d40e4df4c5c728e78328a54da9aa2
Martijn Voncken <mvoncken@gmail.com>
2025-11-11 14:49:04 +0100

    add bubble for palyer-color+tooltip

--- owl20.js
+++ owl20.js
@@ -1,8 +1,8 @@
 import OBR from "@owlbear-rodeo/sdk";
+import he from 'he';
 
-
-
-const renderRoll = (html) =>{
+const renderRoll = (request) =>{
+    const html = `<h3><span title="${he.encode(request.playerName)}" class=playerBubble style="background-color:${request.playerColor}"></span>${he.encode(request.character)}</h3> ${request.html}`;
     const element = document.querySelector("#rolls-list");
     const node = document.createElement("div");
     node.innerHTML = html;
@@ -10,13 +10,16 @@ const renderRoll = (html) =>{
     window.scrollTo(0, document.body.scrollHeight);
 }
 
-const broadcastRoll = (request) =>{
+const broadcastRoll = async (request) => {
     // load sound every time because I want to play 5 at a time if someone 5-clicked.
     const sound = new Audio('dice-89594.mp3');
     sound.load();
     sound.play();      
-    const html = `<h3>${request.character}</h3> ${request.html}`;
-    OBR.broadcast.sendMessage("owl20.roll", html, {destination:"ALL"});
+
+    request.playerColor = await OBR.player.getColor();
+    request.playerName = await OBR.player.getName();
+
+    OBR.broadcast.sendMessage("owl20.roll", request, {destination:"ALL"});
 }
 
 addEventListener("message", (event) => { 
@@ -31,9 +34,6 @@ addEventListener("message", (event) => {
   }
 })
 
-
-
-
 const themeManager  = async () => {
   const setTheme = (theme) => {
       console.log(theme)
@@ -46,8 +46,6 @@ const themeManager  = async () => {
   })
 }
 
-
-  
 OBR.onReady(() => {
   console.log("owl20-owlbear: OBR Ready")
   OBR.broadcast.onMessage("owl20.roll", (event) => {
